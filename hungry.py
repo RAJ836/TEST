80d687dd454b714bcda534d8125d9d416844659d
RAJ836 <raj.annam5@gmail.com>
2020-03-29 13:37:27 +0530

    thirsty

--- hungry.py
+++ hungry.py
@@ -2,4 +2,7 @@ hungry = input("are you hungry : ? ")
 if hungry=='yes':
     print("Eat Food")
 else:
-    print("Do you work")
+    thirsty=input("are you thirsty? ")
+    if thirsty=='yes':
+        print('Drink water')
+    #print("Do you work")
